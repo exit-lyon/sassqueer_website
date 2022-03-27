2e8dd6cf4018effb58c129d0761f1f22d6374534
Xia0ben <10845530+Xia0ben@users.noreply.github.com>
2022-03-27 22:10:46 +0200

    Start adding pics with proper integration using custom carousel.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
-const angleRange = document.getElementById('angle-control');
-const angleResult = document.getElementById('angle-result');
-const root = document.querySelector('HTML');
-const boxes = document.querySelectorAll('.box');
+function moveCarousel(event) {
+  current_card = event.target.parentElement;
+  cards = Array.from(current_card.parentElement.getElementsByClassName("card"));
 
-angleRange.addEventListener("input", function() {
-  const angle = angleRange.value;
-  const angleRad = angle * Math.PI / 180;
-  const magicNumber = Math.abs( Math.tan(angleRad) / 2 );
-  angleResult.innerHTML =  angleRange.value + " deg";
-  document.documentElement.style.setProperty('--angle', angleRange.value + 'deg');
-  document.documentElement.style.setProperty('--magic-number', magicNumber);
-  
-  // Sadly the animation only updates correctly in Firefox :-(
-  boxes.forEach((box) => {
-    box.style.animation = "none";
-  });
-}, false);
+  current_card_index = cards.indexOf(current_card);
+  last_index = cards.length - 1;
+
+  previous_card_index = (current_card_index === 0 ? last_index : current_card_index - 1);
+  next_card_index = (current_card_index === last_index ? 0 : current_card_index + 1)
+
+  other_indices = new Set(cards.keys());
+  other_indices.delete(previous_card_index);
+  other_indices.delete(current_card_index);
+  other_indices.delete(next_card_index);
+
+  cards[current_card_index].classList.add("middle-card");
+  cards[current_card_index].classList.remove("left-card");
+  cards[current_card_index].classList.remove("right-card");
+  cards[current_card_index].classList.remove("hidden-card");
+
+  cards[previous_card_index].classList.add("left-card");
+  cards[previous_card_index].classList.remove("middle-card");
+  cards[previous_card_index].classList.remove("right-card");
+  cards[previous_card_index].classList.remove("hidden-card");
+
+  cards[next_card_index].classList.add("right-card");
+  cards[next_card_index].classList.remove("left-card");
+  cards[next_card_index].classList.remove("middle-card");
+  cards[next_card_index].classList.remove("hidden-card");
+
+  for (const i of other_indices) {
+    cards[i].classList.add("hidden-card");
+    cards[i].classList.remove("left-card");
+    cards[i].classList.remove("right-card");
+    cards[i].classList.remove("middle-card");
+  }
+}
