ffc3d239024c012944874178d48eb7148443bfd8
xia0ben <10845530+Xia0ben@users.noreply.github.com>
2022-03-31 22:37:34 +0200

    Finish carousel code, write planning for arts and science events in a decent format.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-function moveCarousel(event) {
-  current_card = event.target.parentElement;
-  cards = Array.from(current_card.parentElement.getElementsByClassName("card"));
+function moveCarousel(current_card) {
+  const cards = Array.from(current_card.parentElement.getElementsByClassName("card"));
 
-  current_card_index = cards.indexOf(current_card);
-  last_index = cards.length - 1;
+  const current_card_index = cards.indexOf(current_card);
+  const last_index = cards.length - 1;
 
-  previous_card_index = (current_card_index === 0 ? last_index : current_card_index - 1);
-  next_card_index = (current_card_index === last_index ? 0 : current_card_index + 1)
+  const previous_card_index = (current_card_index === 0 ? last_index : current_card_index - 1);
+  const next_card_index = (current_card_index === last_index ? 0 : current_card_index + 1)
 
-  other_indices = new Set(cards.keys());
+  const other_indices = new Set(cards.keys());
   other_indices.delete(previous_card_index);
   other_indices.delete(current_card_index);
   other_indices.delete(next_card_index);
@@ -34,4 +33,32 @@ function moveCarousel(event) {
     cards[i].classList.remove("right-card");
     cards[i].classList.remove("middle-card");
   }
+}
+
+function autoMoveCarousel(card_deck) {
+  const next_card = card_deck.getElementsByClassName("right-card")[0];
+  moveCarousel(next_card);
+}
+
+const card_decks = Array.from(document.getElementsByClassName("cards"));
+const card_deck_to_interval = new Map();
+const card_deck_to_timeout = new Map();
+for (const card_deck of card_decks) {
+  card_deck_to_interval.set(card_deck, setInterval(function () {
+    autoMoveCarousel(card_deck)
+  }, 2000));
+}
+
+function clickCarousel(event) {
+  const current_card = event.target.parentElement;
+  const current_card_deck = current_card.parentElement;
+  moveCarousel(current_card);
+
+  clearInterval(card_deck_to_interval.get(current_card_deck));
+  clearTimeout(card_deck_to_timeout.get(current_card_deck));
+  card_deck_to_timeout.set(current_card_deck, setTimeout(function () {
+    card_deck_to_interval.set(current_card_deck, setInterval(function () {
+      autoMoveCarousel(current_card_deck)
+    }, 2000));
+  }, 10000));
 }
